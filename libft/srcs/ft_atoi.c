adaac241525639d272a4c81c6ed670f1521f4823
Mong-Gu <yhw8258@naver.com>
2020-10-20 00:54:20 +0900

    feat : ft_atoi 함수 구현
    - 그러나 테케 4개 통과 못하는 중

---
+++ libft/srcs/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 00:34:30 by hyun              #+#    #+#             */
+/*   Updated: 2020/10/20 00:53:47 by hyun             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int sign;
+	int value;
+
+	i = 0;
+	sign = 1;
+	value = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == ' '))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			value = value * 10 + str[i] - '0';
+		else
+			break ;
+		i++;
+	}
+	return (sign * value);
+}
