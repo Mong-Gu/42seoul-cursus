894f82bced6e91c57e11718b23bebac950534243
Mong-Gu <yhw8258@naver.com>
2020-10-20 23:54:05 +0900

    fix : strncmp 마지막 테스트 케이스 성공
    unsigned char로 캐스팅해주지 않아서 발생한 문제

--- libft/srcs/ft_strncmp.c
+++ libft/srcs/ft_strncmp.c
@@ -6,23 +6,26 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 00:19:47 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/20 00:22:06 by hyun             ###   ########.fr       */
+/*   Updated: 2020/10/20 23:53:37 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(char *s1, char *s2, size_t len)
+int	ft_strncmp1(const char *s1, const char *s2, size_t len)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < len && (s1[i] != '\0' || s2[i] != '\0'))
+	while (i < len)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		else
-			i++;
+		if (str1[i] != str2[i] || (str1[i] == '\0' || str2[i] == '\0'))
+			return (str1[i] - str2[i]);
+		i++;
 	}
 	return (0);
 }
