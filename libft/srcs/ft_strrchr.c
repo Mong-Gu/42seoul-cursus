a484bfe39a9c18b09c521d5f4987cabeca055e82
Mong-Gu <yhw8258@naver.com>
2020-10-21 18:55:13 +0900

    feat : strrchr 함수 구현 및 테스트 완료

---
+++ libft/srcs/ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/21 18:28:41 by hyun              #+#    #+#             */
+/*   Updated: 2020/10/21 18:54:30 by hyun             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s);
+	i = 0;
+	if (c == '\0')
+		return ((char *)s + len);
+	while(i < len)
+	{
+		if (s[len - i - 1] == c)
+			return ((char *)s + len - i - 1);
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)s + len - i - 1);
+	return (NULL);
+}
