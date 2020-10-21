585f9cdc962931aa63ebea269196e8f17f1a834c
Mong-Gu <yhw8258@naver.com>
2020-10-21 16:13:51 +0900

    fix : norminette 체크

--- libft/srcs/ft_calloc.c
+++ libft/srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 15:48:48 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/21 15:52:04 by hyun             ###   ########.fr       */
+/*   Updated: 2020/10/21 16:12:55 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_calloc(size_t number, size_t size)
 {
 	void	*mem;
+
 	if (!(mem = malloc(number * size)))
 		return (NULL);
 	ft_bzero(mem, (number * size));
--- libft/srcs/ft_memcpy.c
+++ libft/srcs/ft_memcpy.c
@@ -6,21 +6,22 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 15:51:58 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/20 15:57:00 by hyun             ###   ########.fr       */
+/*   Updated: 2020/10/21 16:07:38 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	unsigned char	*tmp_src;
 	unsigned char	*tmp_dst;
 	size_t			n;
 
-	tmp_src = src;
-	tmp_dst = dst;
+	tmp_src = (unsigned char *)src;
+	tmp_dst = (unsigned char *)dst;
 	n = 0;
 	while (n++ < len)
 		*tmp_dst++ = *tmp_src++;
+	return (dst);
 }
