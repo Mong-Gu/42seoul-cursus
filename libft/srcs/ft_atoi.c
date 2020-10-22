292f5c3264ad734992d9fd2d5113178e22ae3fe3
Mong-Gu <yhw8258@naver.com>
2020-10-22 17:05:04 +0900

    refactor : 불필요한 소스코드 줄이기
    - 증감연산자와 삼항연산자를 활용하여 반복문 및 조건문 내부 소스코드 축소
    - 숫자를 변환하는 과정에서 불필요한 조건문 제거

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 00:34:30 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/22 17:01:48 by hyun             ###   ########.fr       */
+/*   Updated: 2020/10/22 17:03:21 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int i;
 	int sign;
@@ -24,18 +24,8 @@ int ft_atoi(const char *str)
 	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == ' '))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] != '\0')
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			value = value * 10 + str[i] - '0';
-		else
-			break ;
-		i++;
-	}
+		sign = str[i++] == '-' ? -1 : 1;
+	while(str[i] >= '0' && str[i] <= '9')
+		value = value * 10 + str[i++] - '0';
 	return (sign * value);
 }
