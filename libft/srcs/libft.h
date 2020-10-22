d9a0382477f4ed6f6783ca9e3e2db0d2b5c21d64
Mong-Gu <yhw8258@naver.com>
2020-10-22 19:29:14 +0900

    chore : 헤더파일 최신화 및 과제 순서에 맞게 함수 선언부 순서 정리

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 17:33:59 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/22 16:20:07 by hyun             ###   ########.fr       */
+/*   Updated: 2020/10/22 19:26:35 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,30 @@
 # include <unistd.h>
 
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(char *src);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t len);
-int		ft_atoi(const char *str);
-void	ft_bzero(void *b, size_t len);
-void	*ft_memset(void *dest, int c, size_t len);
-int		ft_atoi(const char *str);
-void	*ft_calloc(size_t number, size_t size);
-void	*ft_memcpy(void *dst, const void *src, size_t len);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t len);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *b, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t number, size_t size);
+char	*ft_strdup(char *src);
+char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
