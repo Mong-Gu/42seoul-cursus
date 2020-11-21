1c2897198c5d443b47f5a4654902f93cdbb8e699
Hyeonu Yun <hyun@c1r5s7.42seoul.kr>
2020-11-21 18:35:11 +0900

    201121, ft_putbr_fd

--- libft/srcs/ft_abs.c
+++ libft/srcs/ft_abs.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/21 18:11:44 by hyun              #+#    #+#             */
-/*   Updated: 2020/11/21 18:22:20 by hyun             ###   ########.fr       */
+/*   Created: 2020/11/21 17:14:06 by hyun              #+#    #+#             */
+/*   Updated: 2020/11/21 17:15:46 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+int	ft_abs(int nbr)
 {
-	char	str[13];
-	int		is_negative;
-	int		len;
-
-	ft_bzero(str, 13);
-	is_negative = n < 0 ? 1 : 0;
-	len = 0;
-	if (n == 0)
-		str[0] = '0';
-	while (n != 0)
-	{
-		str[len++] = '0' + ft_abs(n % 10);
-		n /= 10;
-	}
-	if (is_negative)
-		str[len] = '-';
-	ft_strrev(str);
-	write(fd, str, ft_strlen(str));
+	return ((nbr < 0) ? -nbr : nbr);
 }
--- libft/srcs/ft_strrev.c
+++ libft/srcs/ft_strrev.c
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/21 18:11:44 by hyun              #+#    #+#             */
-/*   Updated: 2020/11/21 18:22:20 by hyun             ###   ########.fr       */
+/*   Created: 2020/11/21 18:16:02 by hyun              #+#    #+#             */
+/*   Updated: 2020/11/21 18:29:00 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_strrev(char *str)
 {
-	char	str[13];
-	int		is_negative;
-	int		len;
+	size_t	len;
+	size_t	i;
+	char	tmp;
 
-	ft_bzero(str, 13);
-	is_negative = n < 0 ? 1 : 0;
-	len = 0;
-	if (n == 0)
-		str[0] = '0';
-	while (n != 0)
+	len = ft_strlen(str);
+	i = 0;
+	while (i < len / 2)
 	{
-		str[len++] = '0' + ft_abs(n % 10);
-		n /= 10;
+		tmp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = tmp;
+		i++;
 	}
-	if (is_negative)
-		str[len] = '-';
-	ft_strrev(str);
-	write(fd, str, ft_strlen(str));
 }
