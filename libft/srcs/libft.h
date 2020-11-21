25f92683cd6c16500dc0b63156581c62b368d8d6
Hyeonu Yun <hyun@c1r5s7.42seoul.kr>
2020-11-21 18:34:22 +0900

    chore : 헤더파일 최신화

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 17:33:59 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/22 19:26:35 by hyun             ###   ########.fr       */
+/*   Updated: 2020/11/21 18:26:19 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,10 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_abs(int nbr);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void	ft_strrev(char *str);
 void	*ft_memset(void *dest, int c, size_t len);
 void	ft_bzero(void *b, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
