d3d70511bf69e78c74d86ec29d94cab937f3d37d
Mong-Gu <yhw8258@naver.com>
2020-12-05 23:55:08 +0900

    fix: libft-war-machine 통과하기 위해 최대, 최소범위 벗어나는지 판별

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 00:34:30 by hyun              #+#    #+#             */
-/*   Updated: 2020/10/22 17:12:04 by hyun             ###   ########.fr       */
+/*   Updated: 2020/12/05 23:54:03 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int value;
+	int					i;
+	int					sign;
+	unsigned long long	value;
 
 	i = 0;
 	sign = 1;
@@ -27,5 +27,9 @@ int	ft_atoi(const char *str)
 		sign = str[i++] == '-' ? -1 : 1;
 	while (str[i] >= '0' && str[i] <= '9')
 		value = value * 10 + str[i++] - '0';
+	if (sign == 1 && value > INT_MAX)
+		return (-1);
+	if (sign == -1 && value > INT_MIN_ABS)
+		return (0);
 	return (sign * value);
 }
