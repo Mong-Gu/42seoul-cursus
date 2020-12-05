3e5d0fea3da2af4e33428cd26d3a53681dd004c6
Mong-Gu <yhw8258@naver.com>
2020-12-05 19:07:38 +0900

    fix: 리팩토링하면서 norminette 놓쳤던 것 처리, 불필요한 괄호 제거

--- libft/srcs/ft_substr.c
+++ libft/srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 21:59:11 by hyun              #+#    #+#             */
-/*   Updated: 2020/12/05 19:00:46 by hyun             ###   ########.fr       */
+/*   Updated: 2020/12/05 19:07:00 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	slen = ft_strlen(s);
 	if ((size_t)start > slen)
 		return (ft_strdup(""));
-
 	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
-	ptrsize = slen - (size_t)start) < len ? ft_strlcpy(ptr, &s[start], slen - start + 1) : ft_strlcpy(ptr, &s[start], len + 1);
+	ptrsize = slen - (size_t)start < len ?
+		ft_strlcpy(ptr, &s[start], slen - start + 1) :
+		ft_strlcpy(ptr, &s[start], len + 1);
 	return (ptr);
 }
