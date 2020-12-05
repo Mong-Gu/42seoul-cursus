1c2977d72ae90a22999ad512903d6ca3f7022439
Mong-Gu <yhw8258@naver.com>
2020-12-05 18:50:04 +0900

    fix: ft_strtrim 빠져서 다시 반영

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 17:33:59 by hyun              #+#    #+#             */
-/*   Updated: 2020/12/05 18:41:10 by hyun             ###   ########.fr       */
+/*   Updated: 2020/12/05 18:49:34 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_free_array(char **s, int row_cnt);
 char	**ft_split(char const *s, char c);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
