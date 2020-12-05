247e8cb05caf3ef084e87f94d051f83ba4a9d784
Mong-Gu <yhw8258@naver.com>
2020-12-05 19:51:55 +0900

    chore: 추가한 함수 libft에 반영

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 17:33:59 by hyun              #+#    #+#             */
-/*   Updated: 2020/12/05 19:35:41 by hyun             ###   ########.fr       */
+/*   Updated: 2020/12/05 19:40:56 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
